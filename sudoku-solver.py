1ee462ed9d4d633730f3c8efff8cd3de71444ec4
Karthik <sk30102k@gmail.com>
2021-09-25 15:52:09 +0530

    linting

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -8,16 +8,16 @@ puzzle = [[0 for x in range(9)] for y in range(9)]
 def print_grid(p):
     """prints a filled grid using the grid provided
 
-	Args:
-		p ([[int]]): puzzle grid with 0's for empty cells
+        Args:
+                p ([[int]]): puzzle grid with 0's for empty cells
 
-	Returns:
-		int: 0
-	"""
+        Returns:
+                int: 0
+    """
     print("-" * (19 * 2 + 1))
     for i in range(9):
         for j in range(9):
-            print(" | " + str(p[i][j]), end = "")
+            print(" | " + str(p[i][j]), end="")
         print(' |')
         print("-" * (19 * 2 + 1))
     return 0
@@ -27,48 +27,48 @@ def print_grid(p):
 def is_valid(grid, row, col, num):
     """checks sudoku grid for numbers in invalid positions
 
-	Args:
-		grid ([[[int]]]): list of lists of lists of ints
-		row ([[int]]): a list of horizontal rows
-		col ([int]): the horizontal position of ints
-		num (int): the number to check against
+        Args:
+                grid ([[[int]]]): list of lists of lists of ints
+                row ([[int]]): a list of horizontal rows
+                col ([int]): the horizontal position of ints
+                num (int): the number to check against
 
-	Returns:
-		bool: Validity of sudoku grid
-	"""
-	for x in range(9):
-		if grid[row][x] is num:
-			return False
-	for x in range(9):
-		if grid[x][col] is num:
-			return False
-	start_row = row - row % 3
-	start_column = col - col % 3
-	for i in range(3):
-		for j in range(3):
-			if grid[i + start_row][j + start_column] is num:
-				return False
-	return True
+        Returns:
+                bool: Validity of sudoku grid
+    """
+    for x in range(9):
+        if grid[row][x] is num:
+            return False
+        for x in range(9):
+            if grid[x][col] is num:
+                return False
+        start_row = row - row % 3
+        start_column = col - col % 3
+        for i in range(3):
+            for j in range(3):
+                if grid[i + start_row][j + start_column] is num:
+                    return False
+        return True
 
 
 # %%
 def solve_sudoku(grid, row, col):
     """solves grid populated with helper digits
 
-	Args:
-		grid ([[int]]): sudoku grid populated with helpers
-		row (int): initial row number
-		col (int): initial column number
+        Args:
+                grid ([[int]]): sudoku grid populated with helpers
+                row (int): initial row number
+                col (int): initial column number
 
-	Returns:
-		bool: recursively calls the function to check each row for the appropriate entry, then fills it
-	"""
-	if row is 8 and col is 9:
-		return True
+        Returns:
+                bool: recursively calls the function to check each row for the appropriate entry, then fills it
+    """
+    if row == 8 and col == 9:
+        return True
 
-	if col is 9:
-		row += 1
-		col = 0
+    if col == 9:
+        row += 1
+        col = 0
 
     if grid[row][col] > 0:
         return solve_sudoku(grid, row, col + 1)
